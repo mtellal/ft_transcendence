727b66eb4b968a17029f58ef19f332bc2ed8ae57
aessakhi <aessakhi@student.42.fr>
2023-05-03 22:58:33 +0200

    Added channels module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+import { ChannelsModule } from './channels/channels.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule, ChatModule],
+  imports: [PrismaModule, UsersModule, AuthModule, ChatModule, ChannelsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
