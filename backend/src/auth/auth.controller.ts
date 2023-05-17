4b649a1519e7e4cde1123ec2f0b0f7d423a439b2
JB <jbdubillard@hotmail.com>
2023-05-17 16:24:18 +0200

    Merge backend -> jdubilla

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto, SigninDto } from "./dto";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @ApiTags('auth')
 @Controller('auth')
@@ -9,12 +9,14 @@ export class AuthController{
 	constructor(private authService: AuthService) {}
 
 	@Post('signup')
+	@ApiOperation({description: 'Creates a new user with the specified username and password. The username must be unique. Returns a JWT corresponding to the user'})
 	signup(@Body() dto: AuthDto) {
 		return this.authService.signup(dto);
 	}
 	
 	@HttpCode(HttpStatus.OK)
 	@Post('signin')
+	@ApiOperation({description: 'Sign in using an existing user credentials. Returns a JWT corresponding to that user'})
 	signin(@Body() {username, password}: SigninDto) {
 		return this.authService.signin(username, password);
 	}
