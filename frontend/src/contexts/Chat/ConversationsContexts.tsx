980f1c1a3ee8509c9cebeb5a461e25a2add14bc9
Mezyann TELLAL <mtellal@e2r5p14.clusters.42paris.fr>
2023-05-25 10:15:39 +0200

    Integrated Contexts (Friends + Convs) in Chat interface

--- frontend/src/contexts/Chat/ConversationsContexts.tsx
+++ frontend/src/contexts/Chat/ConversationsContexts.tsx
@@ -13,30 +13,35 @@ function newConversation(channel: any) {
 }
 
 function reducer(conversations : any, action : any) {
+    console.log("Conversations reducer called");
     switch(action.type) {
         case('addConv'): {
             return ([...conversations, newConversation(action.conversation)])
         }
         case('initMessages'): {
             const messages = action.messages;
-            conversations.map((c: any) => {
-                if (c.id === messages[0].channelId)
-                {
-                    /* if (currentUser && currentUser.blockedList.length && 
-                            currentUser.blockedList.find((id :any) => id === arrayMessages[0].channeId))
-                            return (c); */
-                    c.messages = messages;                    
-                }
-                return (c);
-            })
+            return (
+                    conversations.map((c: any) => {
+                        if (c.id === messages[0].channelId)
+                        {
+                            /* if (currentUser && currentUser.blockedList.length && 
+                                    currentUser.blockedList.find((id :any) => id === arrayMessages[0].channeId))
+                                    return (c); */
+                            c.messages = messages;                    
+                        }
+                        return (c);
+                    })
+            )
         }
         case('addMessage'): {
             const message = action.message;
-            conversations.map((c: any, i: number) => {
-                if (c.id === message.channelId)
-                    c.messages = [...c.messages, message];
-                return (c);
-            })
+            return (
+                conversations.map((c: any, i: number) => {
+                    if (c.id === message.channelId)
+                        c.messages = [...c.messages, message];
+                    return (c);
+                })
+            )
         }
         default: return (conversations)
     }
@@ -45,7 +50,12 @@ function reducer(conversations : any, action : any) {
 
 export function ConversationsProvider({children} : any)
 {
-    const [conversations, dispatch] : any = useReducer(reducer, 0);
+    const [conversations, dispatch] : any = useReducer(reducer, []);
+
+
+
+
+    
     return (
         <ConversationsContext.Provider value={[conversations, dispatch]}>
             {children}
