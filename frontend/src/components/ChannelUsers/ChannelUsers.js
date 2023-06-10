490d333903a56436efa2f26f4b9c0248ad16081e
jdubilla <jbdubillard@hotmail.com>
2023-06-10 19:43:01 +0200

    rm .js

--- frontend/src/components/ChannelUsers/ChannelUsers.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ChannelUserList } from "../ChannelUserList/ChannelUserList";
-export function ChannelUsers({ dataChannel }) {
-    const usersChan = dataChannel.members;
-    return (React.createElement("div", null, usersChan.map((user) => {
-        return (React.createElement("span", { key: user },
-            React.createElement(ChannelUserList, { user: user, dataChannel: dataChannel })));
-    })));
-}
--- frontend/src/components/HeaderProfile/HeaderProfile.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from './style.module.css';
-export function HeaderProfile({ selector }) {
-    const image = selector.avatar;
-    return (React.createElement("div", { className: s.container },
-        selector.username,
-        React.createElement("img", { className: s.image, src: image })));
-}
