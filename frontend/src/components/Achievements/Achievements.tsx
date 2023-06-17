c2fc34d71cc2d5a7ee7ae08b108162cf73c06829
Jean-baptiste DUBILLARD <jdubilla@e2r10p23.clusters.42paris.fr>
2023-06-17 13:23:07 +0200

    Add icons achievements

--- frontend/src/components/Achievements/Achievements.tsx
+++ frontend/src/components/Achievements/Achievements.tsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { BoxAchievements } from "../BoxAchievements/BoxAchievements";
-import trophy_1 from "../../assets/trophy1.png"
-import trophy_5 from "../../assets/trophy5.png"
+import novice from "../../assets/novice.png"
+import intermediate from "../../assets/intermediate.png"
+import expert from "../../assets/expert.png"
+import master from "../../assets/master.png"
+import fire from "../../assets/fire.png"
+import tenacious from "../../assets/tenacious.png"
+import godlike from "../../assets/godlike.png"
 import s from './style.module.css'
 
 export function Achievements() {
 	return (
 		<div className={s.container}>
-			<BoxAchievements name={'Novice'} description={"Win 1 game"} image={trophy_1}/>
-			<BoxAchievements name={'Intermediaire'} description={"Win 5 games"} image={trophy_5}/>
-			<BoxAchievements name={'Intermediaire'} description={"Win 5 games"} image={trophy_5}/>
-			<BoxAchievements name={'Intermediaire'} description={"Win 5 games"} image={trophy_5}/>
-			<BoxAchievements name={'Intermediaire'} description={"Win 5 games"} image={trophy_5}/>
-			<BoxAchievements name={'Intermediaire'} description={"Win 5 games"} image={trophy_5}/>
-			<BoxAchievements name={'Intermediaire'} description={"Win 5 games"} image={trophy_5}/>
+			<BoxAchievements name={'Novice'} description={"Play 1 Pong match"} image={novice}/>
+			<BoxAchievements name={'Intermediate'} description={"Win 5 Pong matches"} image={intermediate}/>
+			<BoxAchievements name={'Expert'} description={"Reach 10 Pong matches"} image={expert}/>
+			<BoxAchievements name={'Master'} description={"Reach 20 Pong matches"} image={master}/>
+			<BoxAchievements name={'On fire'} description={"Achieve 5 wins in a row"} image={fire}/>
+			<BoxAchievements name={'Tenacisous'} description={"Experience 5 losses in a row"} image={tenacious}/>
+			<BoxAchievements name={'Godlike'} description={"Have 80% win rate on 10 Pong matches"} image={godlike}/>
 		</div>
 	);
 }
