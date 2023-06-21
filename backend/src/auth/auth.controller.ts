b13c4b078ff1eb5e7b243b0baf5edc6335912c4e
Anbarbie <siliose@hotmail.fr>
2023-06-21 17:22:34 +0200

    removing blockguard

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -11,7 +11,6 @@ export class AuthController{
 	constructor(private authService: AuthService) {}
 
 	@Post('signup')
-	@UseGuards(BlockGuard)
 	@ApiOperation({ summary: 'Sign up',
 	description: 'Creates a new user with the specified username and password. The username must be unique. Returns a JWT corresponding to the user' })
 	@ApiBody({type: AuthDto})
@@ -21,7 +20,6 @@ export class AuthController{
 	
 	@HttpCode(HttpStatus.OK)
 	@Post('signin')
-	@UseGuards(BlockGuard)
 	@ApiOperation({ summary: 'Sign in', description: 'Sign in using existing user credentials. Returns a JWT corresponding to that user' })
 	@ApiBody({ type: SigninDto })
 	async signin(@Body() body: SigninDto, @Query('twoFA', ParseBoolPipe) twoFA: boolean) {
@@ -35,7 +33,6 @@ export class AuthController{
 	}
 
 	@Get('42')
-	@UseGuards(BlockGuard)
 	@ApiOperation({ summary: 'ftAuth', description: 'Endpoint description for redirecting to 42 oauth service' })
 	@ApiBearerAuth()
 	@UseGuards(FTOauthGuard)
@@ -58,7 +55,6 @@ export class AuthController{
 	}
 
 	@Post('42/trade')
-	@UseGuards(BlockGuard)
 	@ApiOperation({ summary: 'Trade 42 OAuth_code and 2fa code', description: 'Trade the 42 OAuth code and 2fa code if necessary for a JWT access token' })
 	@ApiBody({ type: TradeDto })
 	async ftAuthTrade(@Body() body: TradeDto) {
