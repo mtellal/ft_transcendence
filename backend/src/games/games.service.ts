0d42057e06f8bbd826032fbcaed01f747df89cfc
aessakhi <aessakhi@student.42.fr>
2023-06-22 12:41:56 +0200

    Delete every game invite on matchmaking start

--- backend/src/games/games.service.ts
+++ backend/src/games/games.service.ts
@@ -490,18 +490,6 @@ export class GamesService {
     return newGame;
   }
 
-  async userhasPendingInvites(userId: number) {
-    const game_exist = await this.prisma.game.findFirst({
-      where: {
-        player1Id: userId,
-        status: GameStatus.INVITE
-      }
-    })
-    if (game_exist)
-      return (true);
-    return (false);
-  }
-
   async acceptInvite(userId: number, gameId: number) {
     const joinedGame = await this.prisma.game.update({
       where: {id: gameId},
