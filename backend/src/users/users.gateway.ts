2df28170aa35a7729b69623dd3656dd915132457
Alexandre ESSAKHI <aessakhi@bess-f4r1s11.clusters.42paris.fr>
2023-06-24 18:21:41 +0200

    Update the userStatus to OFFLINE if he exits the page brutally

--- backend/src/users/users.gateway.ts
+++ backend/src/users/users.gateway.ts
@@ -1,9 +1,10 @@
 import { UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Game, User } from '@prisma/client';
+import { Game, User, Status } from '@prisma/client';
 import { Socket, Server, Namespace } from 'socket.io';
 import { UsersService } from './users.service';
+import { JwtPayloadDto } from '../auth/dto';
 
 @WebSocketGateway({ namespace: 'users' })
 export class UsersGateway implements OnGatewayConnection, OnGatewayDisconnect{
@@ -52,6 +53,31 @@ export class UsersGateway implements OnGatewayConnection, OnGatewayDisconnect{
   }
 
   async handleDisconnect(client: Socket) {
+	const jwtService = this.jwtService;
+    const cookie = client.handshake.headers?.cookie;
+    if (!cookie) {
+      client.disconnect();
+      return ;
+    }
+    const token = cookie.split('=')[1];
+    const decoded: JwtPayloadDto = jwtService.decode(token) as JwtPayloadDto;
+	const user: User = await this.userService.findOne(decoded.id);
+	  if (user.userStatus === Status.ONLINE || user.userStatus === Status.INGAME) {
+		  const updatedUser = await this.userService.update(user.id, {
+			  avatar: undefined,
+			  userStatus: Status.OFFLINE
+		  });
+		  for (const friendId of user.friendList) {
+			  const socketId = this.getSocketId(friendId);
+			  this.io.to(socketId).emit('updatedFriend', updatedUser);
+		  }
+		  for (const channel of updatedUser.channelList) {
+			  this.io.to(channel.toString()).emit('updatedMember', {
+				  channelId: channel,
+				  user: updatedUser
+			  })
+		  }
+	  }
     this.removeSocketId(client.id);
     client.disconnect();
   }
